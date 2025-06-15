e426521e2c748ab24e4fe914e1844a4d274aa784
Ren Tamdang <tamdangren@gmail.com>
2025-06-15 18:27:15 +0800

    Idle animations and movement

--- scripts/scenes/Level1.js
+++ scripts/scenes/Level1.js
@@ -4,7 +4,13 @@ class Level1 extends Phaser.Scene {
     }
 
     init() {
-        this.playerType = null;
+        //** Default starts with Clef's stats
+        // playerType dictates if the player is Clef or Quarter
+        // playerSpeed is their walk/run speed
+        // currentIdleKey is which animation should be active currently depending on which player is active*/ 
+        this.playerType = "Clef";
+        this.playerSpeed = 150;
+        this.currentIdleKey = "clefIdle";
     }
 
     preload() {
@@ -12,8 +18,7 @@ class Level1 extends Phaser.Scene {
     }
 
     create() {
-        this.playerType = "Clef";
-
+        // Clef and Quarter Initialization
         this.clefPlayer = this.physics.add.sprite(0, 0, 'clefIdle').setFrame(0);
         this.clefPlayer.setCollideWorldBounds(true);
         this.clefPlayer.setVisible(true);
@@ -22,9 +27,11 @@ class Level1 extends Phaser.Scene {
         this.quarterPlayer.setCollideWorldBounds(true);
         this.quarterPlayer.setVisible(false);
 
+        // Cursor Keys
         this.cursors = this.input.keyboard.createCursorKeys();
         this.shiftKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SHIFT);
 
+        // Clef Animations
         this.anims.create({
             key: 'clefIdle',
             frames: this.anims.generateFrameNumbers('clefIdle',
@@ -33,10 +40,11 @@ class Level1 extends Phaser.Scene {
                     end: 9
                 }
             ),
-            frameRate: 10,
+            frameRate: 6,
             repeat: -1
         })
 
+        // Quarter Animations
         this.anims.create({
             key: 'quarterIdle',
             frames: this.anims.generateFrameNumbers('quarterIdle',
@@ -49,23 +57,60 @@ class Level1 extends Phaser.Scene {
             repeat: -1
         })
 
+        // Character Switch Event
         this.input.keyboard.on('keydown_E', (event) => {
             if (this.playerType === "Clef") {
+                // Switches to Quarter if player is Clef
                 this.playerType = "Quarter";
                 this.clefPlayer.setVisible(false);
                 this.quarterPlayer.setVisible(true);
+
+                this.currentIdleKey = "quarterIdle"
+                this.playerSpeed = 85;
+
                 console.log(this.playerType);
             } else if (this.playerType === "Quarter") {
+                // Switches to Clef if player is Quarter
                 this.playerType = "Clef"
                 this.clefPlayer.setVisible(true);
                 this.quarterPlayer.setVisible(false);
+
+                this.currentIdleKey = "clefIdle"
+                this.playerSpeed = 150;
+
                 console.log(this.playerType);
             }
-            
         });
     }
 
     update() {
-
+        switch (this.playerType) {
+            case "Clef":
+                // Clef Movement and Animations
+                if (this.cursors.left.isDown) {
+                    this.clefPlayer.setVelocityX(-this.playerSpeed);
+                    this.quarterPlayer.setVelocityX(-this.playerSpeed);
+                } else if (this.cursors.right.isDown) {
+                    this.clefPlayer.setVelocityX(this.playerSpeed);
+                    this.quarterPlayer.setVelocityX(this.playerSpeed);
+                } else {
+                    this.quarterPlayer.setVelocityX(0);
+                    this.clefPlayer.setVelocityX(0);
+                    this.clefPlayer.anims.play(this.currentIdleKey, true);
+                }
+            case "Quarter":
+                // Quarter Movement and Animations
+                if (this.cursors.left.isDown) {
+                    this.clefPlayer.setVelocityX(-this.playerSpeed);
+                    this.quarterPlayer.setVelocityX(-this.playerSpeed);
+                } else if (this.cursors.right.isDown) {
+                    this.clefPlayer.setVelocityX(this.playerSpeed);
+                    this.quarterPlayer.setVelocityX(this.playerSpeed);
+                } else {
+                    this.clefPlayer.setVelocityX(0);
+                    this.quarterPlayer.setVelocityX(0);
+                    this.quarterPlayer.anims.play(this.currentIdleKey, true);
+                }
+        }
     }
 }
