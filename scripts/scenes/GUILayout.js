fe38f0c710d67cbe58d0ffa9ebd043bead1f49d3
Ren Tamdang <tamdangren@gmail.com>
2025-06-25 14:05:05 +0800

    Implementing chord functionality

--- scripts/scenes/GUILayout.js
+++ scripts/scenes/GUILayout.js
@@ -15,11 +15,17 @@ class GUILayout extends Phaser.Scene{
 
     create() {
         this.livesText = this.add.text(20,20,"Lives: 3");
+        this.chordsText = this.add.text(20,40,`Chords: 0`);
 
         emitter.on('lives-damage',this.livesDown,this);
+        emitter.on('chord-collected',this.chordsUp,this);
     }
 
     livesDown(lives) {
         this.livesText.setText(`Lives: ${lives}`);
+    }
+
+    chordsUp(chordsCollected) {
+        this.chordsText.setText(`Chords: ${chordsCollected}`);
     }
 }
