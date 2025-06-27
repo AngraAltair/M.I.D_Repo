97e89df229206c47eb2a72e60ee3005f3b7b747c
Ren Tamdang <tamdangren@gmail.com>
2025-06-27 14:45:30 +0800

    GUI now uses bigger portraits, HP bar border implemented, and text placed

--- scripts/scenes/GUILayout.js
+++ scripts/scenes/GUILayout.js
@@ -15,18 +15,20 @@ class GUILayout extends Phaser.Scene{
 
     preload() {
         // UI Assets
-        this.load.spritesheet('hpBar','StarBleuGameUi/CommonUi/hpBar(smallSize)(eachIs72x8).png',
-            {frameWidth: 72, frameHeight: 8}
+        this.load.spritesheet('hpBar','StarBleuGameUi/CommonUi/hpBar(bigSize)(eachIs144x16).png',
+            {frameWidth: 144, frameHeight: 16}
         );
         // UI Portraits for when Clef is Active
-        this.load.image('clefBigActive','StarBleuGameUi/ClefUi/Clef(bigPortrait)(smallSize).png');
-        this.load.image('clefSmallActive','StarBleuGameUi/ClefUi/Clef(smallPortrait)(smallSize).png');
-        this.load.image('clefChordActive','StarBleuGameUi/ClefUi/Chord(Clef)(smallSize).png');
+        this.load.image('clefBigActive','StarBleuGameUi/ClefUi/Clef(bigPortrait)(bigSize).png');
+        this.load.image('clefSmallActive','StarBleuGameUi/ClefUi/Clef(smallPortrait)(bigSize).png');
+        this.load.image('clefChordActive','StarBleuGameUi/ClefUi/Chord(Clef)(bigSize).png');
+        this.load.image('clefHpActive','StarBleuGameUi/ClefUi/HPBarBox(Clef)(bigSize).png')
 
         // UI Portraits for when Quarter is Active
-        this.load.image('quarterBigActive','StarBleuGameUi/QuarterUi/Quarter(bigPortrait)(smallSize).png');
-        this.load.image('quarterSmallActive','StarBleuGameUi/QuarterUi/Clef(smallPortrait)(smallSize).png');
-        this.load.image('quarterChordActive','StarBleuGameUi/QuarterUi/Chord(Quarter)(smallSize).png')
+        this.load.image('quarterBigActive','StarBleuGameUi/QuarterUi/Quarter(bigPortrait)(bigSize).png');
+        this.load.image('quarterSmallActive','StarBleuGameUi/QuarterUi/Clef(smallPortrait)(bigSize).png');
+        this.load.image('quarterChordActive','StarBleuGameUi/QuarterUi/Chord(Quarter)(bigSize).png');
+        this.load.image('quarterHpActive','StarBleuGameUi/QuarterUi/HPBarBox(Quarter)(bigSize).png');
 
         this.load.image('pauseButton','StarBleuGameUi/CommonUi/pause32x32.png');
         this.load.image('pauseWindow','StarBleuGameUi/PauseMenuUi/pausedHolderTexted283x306.png');
@@ -40,8 +42,14 @@ class GUILayout extends Phaser.Scene{
         this.scene.bringToTop("GUILayout");
 
         this.activePlayerPortrait = this.add.image(20,20,'clefBigActive').setOrigin(0,0);
-        this.subPlayerPortrait = this.add.image(80,37,'clefSmallActive').setOrigin(0,0);
-        this.chordPortrait = this.add.image(120,37,'clefChordActive').setOrigin(0,0);
+        this.subPlayerPortrait = this.add.image(120,53,'clefSmallActive').setOrigin(0,0);
+        this.chordPortrait = this.add.image(190,53,'clefChordActive').setOrigin(0,0);
+        
+        this.hpBarBorder = this.add.sprite(120,20,'clefHpActive').setOrigin(0,0);
+        this.hpBar = this.add.sprite(128,28,'hpBar').setOrigin(0,0).setFrame(3);
+        
+        this.chordsText = this.add.text(220,120,`0`);
+        this.add.text(128,120,"[ 2 ]");
 
         this.pauseWindow = this.add.image(325, 200, 'pauseWindow').setVisible(false);
 
@@ -71,9 +79,6 @@ class GUILayout extends Phaser.Scene{
             useHandCursor: true
         });
 
-        this.hpBar = this.add.sprite(80,20,'hpBar').setOrigin(0,0).setFrame(3);
-
-        this.chordsText = this.add.text(160,45,`0`);
 
         emitter.on('lives-damage',this.livesDown,this);
         emitter.on('chord-collected',this.chordsUp,this);
@@ -96,10 +101,12 @@ class GUILayout extends Phaser.Scene{
             this.activePlayerPortrait.setTexture('clefBigActive');
             this.subPlayerPortrait.setTexture('clefSmallActive');
             this.chordPortrait.setTexture('clefChordActive');
+            this.hpBarBorder.setTexture('clefHpActive');
         } else if (playerType == "Quarter") {
             this.activePlayerPortrait.setTexture('quarterBigActive');
             this.subPlayerPortrait.setTexture('quarterSmallActive');
             this.chordPortrait.setTexture('quarterChordActive');
+            this.hpBarBorder.setTexture('quarterHpActive');
         }
         console.log(playerType);
     }
